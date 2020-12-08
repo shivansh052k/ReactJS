7bd4434898697f913946bef6d6655ae5f206a060
Shivansh Gupta <shivanshgupta323@gmail.com>
2020-12-09 00:08:03 +0530

    Add files via upload

---
+++ exp1/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './components/Greet'
+import Greet from './components/Greet';
+
+function App() {
+  return (
+    <div className="App">
+      <Greet />
+    </div>
+  );
+}
+
+export default App;
