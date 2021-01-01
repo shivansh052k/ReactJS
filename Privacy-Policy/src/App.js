bfd0018671ba1b36c5ba588d368d05fb911bf312
Shivansh Gupta <shivanshgupta323@gmail.com>
2021-01-01 23:28:40 +0530

    Add files via upload

---
+++ Privacy-Policy/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Text from './component/Text';
+import Privacy from './component/Privacy';
+import Joinbar from './component/Joinbar';
+
+function App() {
+  return (
+    <>
+    
+    <Text />
+    <Privacy />
+    <Joinbar />
+    
+    </>
+  );
+}
+
+export default App;
