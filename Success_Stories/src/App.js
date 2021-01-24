72ba324039daccf9adaddbd6e48f94e9c1c2ea65
Shivansh Gupta <shivanshgupta323@gmail.com>
2021-01-24 23:15:04 +0530

    Add files via upload

---
+++ Success_Stories/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Text from './component/Text';
+import SuccessStories from './component/SuccessStories';
+
+function App() {
+  return (
+    <>
+    
+    <Text />
+    <SuccessStories />
+    
+    </>
+  );
+}
+
+export default App;
