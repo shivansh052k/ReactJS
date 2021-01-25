6b8a85b863f4693bc4451e2f50be8156d8a33752
Shivansh Gupta <shivanshgupta323@gmail.com>
2021-01-25 16:39:51 +0530

    Add files via upload

---
+++ About_Us/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Text from './component/Text';
+import AboutUs from './component/AboutUs';
+import Journeybar from './component/Journeybar';
+
+function App() {
+  return (
+    <>
+    
+    <Text />
+    <AboutUs />
+    <Journeybar />
+    
+    </>
+  );
+}
+
+export default App;
