25c7042f83c8fc2a3deb68f6dd3693090ba8f17f
Shivansh Gupta <shivanshgupta323@gmail.com>
2021-02-07 16:12:26 +0530

    Add files via upload

---
+++ Young_Minds/Blogs_Single_Page/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Text from './component/Text';
+import SinglePage from './component/SinglePage';
+
+function App() {
+  return (
+    <>
+    
+    <Text />
+    <SinglePage />
+    
+    </>
+  );
+}
+
+export default App;
