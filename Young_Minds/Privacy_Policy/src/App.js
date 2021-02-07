9b37a23d3c7cd24fa07efab9153f2d97d7369b7f
Shivansh Gupta <shivanshgupta323@gmail.com>
2021-02-07 15:58:50 +0530

    Add files via upload

---
+++ Young_Minds/Privacy_Policy/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Text from './component/Text';
+import Privacy from './component/Privacy';
+
+
+function App() {
+  return (
+    <>
+    
+    <Text />
+    <Privacy />
+
+    
+    </>
+  );
+}
+
+export default App;
